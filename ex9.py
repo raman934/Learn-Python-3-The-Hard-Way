bf76ede6b6f2f1ec82d6479ac1f6ec8ad5ba1399
Raman Gandhi <ramangandhi392000@gmail.com>
2020-04-09 21:34:40 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"
+
+print("Here are the days : ", days)
+print("Here are the months : ", months)
+
+print("""
+WE CAN WRITE AS MUCH AS WE WANT IN
+THREE DOUBLEE-QUOTES
+EVEN 4 LINES OR MORE
+""")
